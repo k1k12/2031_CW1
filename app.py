82aaae309ebee094edf3cb33ff31d2e96d517ecc
k1k12 <sofiafrench@sky.com>
2024-10-14 13:54:21 +0100

    Remove redundant code

--- app.py
+++ app.py
@@ -8,41 +8,6 @@ from flask import render_template
 def index():
     return render_template('home/index.html')
 
-# Load webpage 'registration' into browser
-@app.route('/registration')
-def registration():
-    return render_template('accounts/registration.html')
-
-# Load webpage 'login' into browser
-@app.route('/login')
-def login():
-    return render_template('accounts/login.html')
-
-# Load webpage 'account' into browser
-@app.route('/account')
-def account():
-    return render_template('accounts/account.html')
-
-# Load webpage 'posts' into browser
-@app.route('/posts')
-def posts():
-    return render_template('posts/posts.html')
-
-# Load webpage 'create' into browser
-@app.route('/create')
-def create():
-    return render_template('posts/create.html')
-
-# Load webpage 'update' into browser
-@app.route('/update')
-def update():
-    return render_template('posts/update.html')
-
-# Load webpage 'security' into browser
-@app.route('/security')
-def security():
-    return render_template('security/security.html')
-
 # Main method
 if __name__ == '__main__':
     app.run()
