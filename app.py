921db2b4e7f0569777fcbb90aa99b461f2191d3c
k1k12 <sofiafrench@sky.com>
2024-10-14 13:04:07 +0100

    Load all webpages

--- app.py
+++ app.py
@@ -13,6 +13,36 @@ def index():
 def registration():
     return render_template('accounts/registration.html')
 
+# Load webpage 'login' into browser
+@app.route('/login')
+def registration():
+    return render_template('accounts/login.html')
+
+# Load webpage 'account' into browser
+@app.route('/account')
+def registration():
+    return render_template('accounts/account.html')
+
+# Load webpage 'posts' into browser
+@app.route('/posts')
+def registration():
+    return render_template('posts/posts.html')
+
+# Load webpage 'create' into browser
+@app.route('/create')
+def registration():
+    return render_template('posts/create.html')
+
+# Load webpage 'update' into browser
+@app.route('/update')
+def registration():
+    return render_template('posts/update.html')
+
+# Load webpage 'security' into browser
+@app.route('/security')
+def registration():
+    return render_template('security/security.html')
+
 # Main method
 if __name__ == '__main__':
     app.run()
