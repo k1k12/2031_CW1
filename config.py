d17066552f8c851257a70904ee55ae29836558a2
k1k12 <sofiafrench@sky.com>
2024-11-20 10:07:25 +0000

    Complete excersise 4

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Imports
 
-from flask import Flask
+from flask import Flask, url_for
 from accounts.views import accounts_bp
 from posts.views import posts_bp
 from security.views import security_bp
@@ -12,11 +12,19 @@ from flask_migrate import Migrate
 from sqlalchemy import MetaData
 from datetime import datetime
 from datetime import datetime
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_admin.menu import MenuLink
+import secrets
 
 # Define app
 
 app = Flask(__name__)
 app.debug = True
+
+# Create secret key for client server interaction
+
+app.config['SECRET_KEY'] = secrets.token_hex(16)
 
 # Create config attributes
 
@@ -59,6 +67,32 @@ class Post(db.Model):
         self.created = datetime.now()
         self.title = title
         self.body = body
+
+# Db admin page template
+
+class MainIndexLink(MenuLink):
+    def get_url(self):
+        return url_for('index')       
+
+
+# Override model view class with post view
+
+class PostView(ModelView):
+    column_display_pk = True  
+    column_hide_backrefs = False
+    column_list = ('id', 'created', 'title', 'body')
+
+# Create admin instance
+admin = Admin(app, name='DB Admin', template_mode='bootstrap4')
+
+# Remove link to db admin home
+admin._menu = admin._menu[1:] 
+
+# Add link to blog home page
+admin.add_link(MainIndexLink(name='Home Page'))
+
+# To view data on posts table
+admin.add_view(PostView(Post, db.session))
 
 # Register blueprints
 
